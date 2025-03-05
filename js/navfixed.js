7cf84af29f4cbf888dd8f4be35512b6a0c515a95
Lu56-prog <lucerocardona167@gmail.com>
2025-03-05 10:27:35 -0500

    guardar

---
+++ js/navfixed.js
@@ -0,0 +1,35 @@
+// navbar principal
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/html/Pagina Principal/nav-principal.html")  
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("nav-principal").innerHTML = data;
+          const navbar = document.querySelector(".navbar");
+          window.addEventListener("scroll", () => {
+              if (window.scrollY > 80) {
+                  navbar.classList.add("fixed");
+              } else {
+                  navbar.classList.remove("fixed");
+              }
+          });
+      })
+      .catch(err => console.error('Error cargando el navbar:', err));
+});
+
+//navbar trabajo
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/html/Pagina Trabajo (Pascual Work)/navbar-trabajo.html")  
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("nav-trabajo").innerHTML = data;
+          const navbar = document.querySelector(".navbar");
+          window.addEventListener("scroll", () => {
+              if (window.scrollY > 80) {
+                  navbar.classList.add("fixed");
+              } else {
+                  navbar.classList.remove("fixed");
+              }
+          });
+      })
+      .catch(err => console.error('Error cargando el navbar:', err));
+});
